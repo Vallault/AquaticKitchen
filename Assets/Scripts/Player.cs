24458e6221ce0882ec2e52a7885c064395b1f455
Vallault <agundoganli1@myjmc.edu.au>
2026-02-05 18:03:06 +1100

    Interactions Part 2
    - Tried to get the player to interact with the Counter.
    Continuing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,34 @@ public class Player : MonoBehaviour
     [SerializeField] private float rotateSpeed = 720f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private Transform visual;
+    [SerializeField] private float interactDistance = 2f;
+    [SerializeField] private float interactRange = 09f;
 
     private Rigidbody rb;
     private bool isWalking;
+    private Vector3 lastInteractionDir = Vector3.forward;
+    private ClearCounter selectedCounter;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void OnDestory()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        if (selectedCounter == null)
+        {
+            Debug.Log("E pressed: no counter selected");
+                return;
+        }
+
+        selectedCounter.Interact();
+     }
 
     private void Awake()
     {
@@ -37,7 +62,10 @@ public class Player : MonoBehaviour
         }
 
         isWalking = moveDir.sqrMagnitude > 0.0001f;
+    }
 
+    private void Update()
+    {
         HandleInteractions();
     }
 
@@ -48,12 +76,42 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        if (moveDir.sqrMagnitude < 0.0001f)
-            return;
+        if (moveDir.sqrMagnitude > 0.0001f)
+        {
+            lastInteractionDir = moveDir;
+        }
 
         float interactDistance = 2f;
         Vector3 rayOrigin = transform.position + Vector3.up * 0.5f;
+        Debug.DrawRay(rayOrigin, lastInteractionDir * interactDistance);
 
-        if (Physics.Raycast(rayOrigin, moveDir, out RaycastHit raycastHit, interactDistance));
+        ClearCounter newSelected = null;
+
+        if (Physics.Raycast(rayOrigin, lastInteractionDir, out RaycastHit raycastHit, interactDistance))
+        {
+            ClearCounter clearCounter = raycastHit.transform.GetComponentInParent<ClearCounter>();
+
+            if (clearCounter != null)
+            {
+                float flatDistance = Vector3.Distance(new Vector3(transform.position.x, 0f, transform.position.z),
+                    new Vector3(clearCounter.transform.position.x, 0f, clearCounter.transform.position.z));
+
+                if (flatDistance <= interactRange)
+                {
+                    newSelected = clearCounter;
+                }
+            }
+
+        }
+
+        if (newSelected != selectedCounter)
+        {
+            selectedCounter = newSelected;
+
+            if (selectedCounter != null)
+                Debug.Log("LOCKED ON: " + selectedCounter.name);
+            else
+                Debug.Log("UNSELECTED");
+        }
     }
 }
